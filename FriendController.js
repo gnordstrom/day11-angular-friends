91cff1b8c77521051c8c2e74873654c494332c0b
Gustav Nordstrom <gnordstrom@users.noreply.github.com>
2016-08-29 15:39:34 -0600

    setup projecT

---
+++ FriendController.js
@@ -0,0 +1,3 @@
+angular.module("angularFriends").controller("FriendController", function($scope){
+  $scope.test = "Hello World";
+});
---
+++ js/mainCtrl.js
@@ -0,0 +1,3 @@
+angular.module("angularFriends").controller("mainCtrl", function($scope){
+  $scope.test = "Testing";
+});
